52bec876b2a248f7195edf4a56aba0b36c9807e8
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-10-24 21:09:47 +0100

    implementation of parameterized test

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -1,7 +1,10 @@
 package com.cbfacademy;
 
-public class FizzBuzz(){
+public class FizzBuzz{
 
-    
+ 
+    public static String of(int value){
+       throw new RuntimeException("method not implemented");
+    }
 
 }
