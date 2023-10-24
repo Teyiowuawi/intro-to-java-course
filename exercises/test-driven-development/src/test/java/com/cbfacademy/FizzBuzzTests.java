2dd31f42334b43b6c4859b38f6bcdb9230335665
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-10-24 20:51:13 +0100

    complete implementation of parameterized test

--- exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTests.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTests.java
@@ -3,14 +3,19 @@ package com.cbfacademy;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 @DisplayName("Should return a number, Fizz, Buzz or FizzBuzz")
 public class FizzBuzzTests {
     
-    static Stream<Arguments> numberToTest(){
+    static Stream<Arguments> numbersToTest(){
         return Stream.of(
             arguments(1, "1"),
             arguments(2, "2"),
@@ -19,5 +24,12 @@ public class FizzBuzzTests {
     }
 
     @ParameterizedTest
+    @MethodSource("numbersToTest")
+    @DisplayName("return expected value")
+    public void returnExpectedValue(Integer number, String expected){
+        final String actual = FizzBuzz.of(number);
+        assertThat(actual, is(expected));
+    }
+
 
 }
